c3a79eda64906116b98884dda7464a65140f9d32
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-11-20 17:45:30 +0530

    Fix bug in login functionality

---
+++ src/components/About.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import {me} from '../assets/index'
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+<div className="flex">
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Embarking on my journey into the realm of programming, I've cultivated a diverse skill set that reflects my passion for technology and innovation. Proficient in Java and Python, I've honed my programming prowess to tackle a variety of challenges. My exploration into the intricacies of deep learning and neural networks has allowed me to delve into the fascinating world of artificial intelligence, where I've gained hands-on experience in creating intelligent systems. Additionally, my expertise extends to the realm of Python performance optimization, where I've fine-tuned code for efficiency and speed. This journey represents my commitment to continuous learning and my dedication to mastering cutting-edge technologies. I am excited to contribute my skills to the ever-evolving landscape of technology.
+      </motion.p>
+      <img src={me} alt="my image" className="w-[200px] h-auto ml-10 mt-8 rounded-full"/>
+  
+      </div>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
